2164b2c04f4c16fe3586080ff826fed22afb6aee
hdnecli <hdnecli@gmail.com>
2024-09-15 23:05:46 +0300

    rapor notu function is added in Rapor Screen.

--- wwwroot/js/rapor.js
+++ wwwroot/js/rapor.js
@@ -5,3 +5,13 @@ function openTani() {
     const url = `/Tani/Index?hastaNo=${hastaNo}`;
     window.location.href = url;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textarea = document.getElementById('rapor-notu');
+    const charCountDisplay = document.getElementById('char-count');
+    
+    textarea.addEventListener('input', function() {
+        const charCount = this.value.length;
+        charCountDisplay.textContent = `${charCount} / 3000`;
+    });
+});
